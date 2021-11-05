69c94ebb3c6dfa4c4a5f36a5e9483a457662e129
ht0m <hartht@gmail.com>
2021-11-05 06:12:50 +0100

    Maxprofit

---
+++ src/main/java/day03/Stock.java
@@ -0,0 +1,41 @@
+package day03;
+
+import day01.Actor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Stock {
+    List<Double> stock = new ArrayList<>();
+    double minValue;
+    double maxValue;
+    double maxProfit;
+
+    public Stock(List<Double> stock) {
+        this.stock = stock;
+    }
+
+    public List<Double> getStock() {
+        return stock;
+    }
+
+    public double maxProfit() {
+
+        for (int i = 0; i < stock.size(); i++) {
+            if (stock.get(i) <= minValue || i==0 ){
+                if (maxProfit < maxValue - minValue) {
+                  maxProfit = maxValue - minValue;
+                }
+                minValue = stock.get(i);
+                maxValue = 0;
+            }
+            if (stock.get(i) > maxValue) {
+                maxValue = stock.get(i);
+            }
+        }
+        if (maxProfit < maxValue - minValue) {
+            maxProfit = maxValue - minValue;
+        }
+        return maxProfit;
+    }
+}
